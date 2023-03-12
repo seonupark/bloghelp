f7cac0080e4c529a09958a8ffeb17e158c7970e7
soenhome <seonu1031@naver.com>
2023-03-13 03:50:36 +0900

    blog1

---
+++ excel.py
@@ -0,0 +1,9 @@
+from openpyxl import load_workbook
+
+aa = load_workbook(filename='practice.xlsx', read_only=True)
+ws = aa['3단']
+
+for row in ws.rows:
+    for cell in row:
+        print(cell.value)
+
