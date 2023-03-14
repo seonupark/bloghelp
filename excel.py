287fb71732c08661ff6012f8760dea74301231ba
soenhome <seonu1031@naver.com>
2023-03-15 04:03:36 +0900

    blog2 insert cell

--- excel.py
+++ excel.py
@@ -1,9 +1,19 @@
 from openpyxl import load_workbook
 
-aa = load_workbook(filename='practice.xlsx', read_only=True)
+aa = load_workbook(filename='practice.xlsx')
 ws = aa['3단']
 
-for row in ws.rows:
-    for cell in row:
-        print(cell.value)
-#git 수정
+ws['B1']=4
+
+for j in range(2,6):
+    for i in range(1,11):
+        ws.cell(column=j,row=i, value=(j+2)*i)
+
+aa.save('practice.xlsx')
+aa.close()
+
+# for row in ws.rows:
+#     for cell in row:
+#         print(cell.value)
+#
+
