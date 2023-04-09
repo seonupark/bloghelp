3c82be16e4c124e10afaf03777903ee90dbdde02
soenhome <seonu1031@naver.com>
2023-04-10 02:46:54 +0900

    merge

--- excel.py
+++ excel.py
@@ -1,9 +1,18 @@
 from openpyxl import load_workbook
+from openpyxl.styles import Alignment
 
 wb = load_workbook(filename='function.xlsx')
 ws = wb['Sheet1']
 
-ws.insert_columns(1,2)
+# 셀 병합
+# ws.merge_cells('A8:E10')
+ws.cell(column=1,row=8, value="제목")
+
+# 셀 병합 후, 병합된 셀 내부의 텍스트를 중앙에 위치시키기 위해 Alignment 객체 생성
+align = Alignment(horizontal='center' , vertical='center')
+
+# 병합된 셀에 스타일 지정
+ws['A8'].alignment = align
 
 wb.save('function.xlsx')
 wb.close()
